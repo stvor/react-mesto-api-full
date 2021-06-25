dd86cefc44def58b051bd51b06732c2a7cb8b224
Stas Voronin <jameslebron@yandex.ru>
2021-06-26 01:24:55 +0300

    fix: при обновлении аватара поле avatar обязательно

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -24,7 +24,7 @@ router.patch('/me', celebrate({
 }), updateUser);
 router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string(),
+    avatar: Joi.string().required(),
   }),
 }), updateAvatar);
 
