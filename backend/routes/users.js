030a67e16fd95c3b31191f68559a3007927c2a90
Stas Voronin <jameslebron@yandex.ru>
2021-06-26 01:03:26 +0300

    fix: при обновлении пользователя оба поля обязательны

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -18,8 +18,8 @@ router.get('/:userId', celebrate({
 }), getUserById);
 router.patch('/me', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
+    name: Joi.string().min(2).max(30).required(),
+    about: Joi.string().min(2).max(30).required(),
   }),
 }), updateUser);
 router.patch('/me/avatar', celebrate({
