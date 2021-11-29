4fbbd670f49e235cffea77856b99d43ae979eb19
Stas Voronin <jameslebron@yandex.ru>
2021-11-29 22:26:58 +0300

    refactor: cleanup

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -23,39 +23,37 @@ function Login({ onLogin }) {
 
   return (
     <main className="content page__content">
-      {/* <section className="content__login login"> */}
-        <form
-          className="form form_type_login"
-          onSubmit={handleSubmit}
-          name="login"
-          action="#"
-          noValidate
-        >
-          <h1 className="form__title form__title_type_login">Вход</h1>
-          <input
-            className="form__input form__input_type_login form__input_type_email"
-            value={email || ''}
-            onChange={handleEmailChange}
-            id="email-input"
-            type="text"
-            name="email"
-            placeholder="Email"
-          />
-          <input
-            className="form__input form__input_type_login form__input_type_password"
-            value={password || ''}
-            onChange={handlePasswordChange}
-            id="password-input"
-            type="password"
-            name="password"
-            placeholder="Пароль"
-          />
-          <button
-            className="form__submit form__submit_type_login"
-            type="submit"
-          >Войти</button>
-        </form>
-      {/* </section> */}
+      <form
+        className="form"
+        onSubmit={handleSubmit}
+        name="login"
+        action="#"
+        noValidate
+      >
+        <h1 className="form__title form__title_type_login">Вход</h1>
+        <input
+          className="form__input form__input_type_login form__input_type_email"
+          value={email || ''}
+          onChange={handleEmailChange}
+          id="email-input"
+          type="text"
+          name="email"
+          placeholder="Email"
+        />
+        <input
+          className="form__input form__input_type_login form__input_type_password"
+          value={password || ''}
+          onChange={handlePasswordChange}
+          id="password-input"
+          type="password"
+          name="password"
+          placeholder="Пароль"
+        />
+        <button
+          className="form__submit form__submit_type_login"
+          type="submit"
+        >Войти</button>
+      </form>
     </main>
   );
 }
